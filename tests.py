434c0a244ae4a49ed661581f559f41ae802d0209
xtraswed <jotarocujoh847@gmail.com>
2024-01-29 02:12:01 +0200

    Updated users.py and client.py. Added guilds

--- tests.py
+++ tests.py
@@ -6,8 +6,8 @@ bot.start()
 us = users(bot)
 
 
-user_id  = us.get_by_nick('janek').user_id
-username = us.get_by_nick('janek').username
-level = us.get_by_nick('janek').level
-guild = us.get_by_nick('janek').guild
+user_id  = us.get_by_nick('xtrafrancyz').user_id
+username = us.get_by_nick('xtrafrancyz').username
+level = us.get_by_nick('xtrafrancyz').level
+guild = us.get_by_nick('xtrafrancyz').guild_id
 print(f'{user_id}\n{username}\n{level}\n{guild}')
--- vimeworld/users.py
+++ vimeworld/users.py
@@ -3,9 +3,9 @@ import json
 from .client import Client
 
 class users():
-	"""docstring for user"""
+	""""""
 	class users():
-		def __init__(self, user_id, username, level, rank, playedSeconds, lastSeen, guild):
+		def __init__(self, user_id, username, level, rank, playedSeconds, lastSeen, guild, guild_id):
 			self.user_id = user_id
 			self.username = username
 			self.level = level
@@ -13,10 +13,11 @@ class users():
 			self.playedSeconds = playedSeconds
 			self.lastSeen = lastSeen
 			self.guild = guild
-	
+			self.guild_id = guild_id
+
 	def __init__(self, client_instance):
 		self.sess = client_instance.session
-	# "username":"xtrafrancyz","level":43,"levelPercentage":0.61355,"rank":"ADMIN","playedSeconds":10948983,"lastSeen":1706281819,"guild":{"id":401,"name":"\u041a\u0435\u043a \u0434\u0435\u043b\u0430 - \u0445\u043e\u0440\u043e\u0448\u043e","tag":null,"color":"&f","level":1,"levelPercentage":0.41668,"avatar_url":"https://mc.vimeworld.com/launcher/guilds/401.png?t=1598021991"}}]
+	
 	def get_by_nick(self, nick):
 		info_by_nick = json.loads(self.sess.get(f"https://api.vimeworld.com/user/name/{nick}").content)
 		if info_by_nick != []:
@@ -28,6 +29,30 @@ class users():
 			playedSeconds = info_by_nick[0].get('playedSeconds', None)
 			lastSeen = info_by_nick[0].get('lastSeen', None)
 			guild = info_by_nick[0].get('guild', None)
-			return self.users(user_id, username, level, rank, playedSeconds, lastSeen, guild)
+			if guild is not None:
+				guild_id = guild.get('id', None)
+			else:
+				guild_id = None
+			return self.users(user_id, username, level, rank, playedSeconds, lastSeen, guild, guild_id)
 		else:
-			return self.users('Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found')
+			return self.users('Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found')
+
+	def get_by_id(self, id:int):
+		info_by_id = json.loads(self.sess.get(f"https://api.vimeworld.com/user/{id}").content)
+		if info_by_id != []:
+			user_id = info_by_id[0].get('id', None)
+			username = info_by_id[0].get('username', None)
+			level = info_by_id[0].get('level', None)
+			levelPercentage = info_by_id[0].get('levelPercentage', None)
+			rank = info_by_id[0].get('rank', None)
+			playedSeconds = info_by_id[0].get('playedSeconds', None)
+			lastSeen = info_by_id[0].get('lastSeen', None)
+			guild = info_by_id[0].get('guild', None)
+			if guild is not None:
+				guild_id = guild.get('id', None)
+			else:
+				guild_id = None
+			return self.users(user_id, username, level, rank, playedSeconds, lastSeen, guild, guild_id)
+		else:
+			return self.users('Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found', 'Not found')
+
