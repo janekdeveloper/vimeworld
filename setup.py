e802ebd026762b08794d803671c1aab62af12b91
xtraswed <jotarocujoh847@gmail.com>
2024-01-30 03:16:58 +0200

    Added setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 
-with open("README.md", "r") as readme_file:
+with open("README.md", "r", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 
