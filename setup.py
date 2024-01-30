11b2cdb30324f8d905e8703ac8717cad94b21a29
xtraswed <jotarocujoh847@gmail.com>
2024-01-30 03:09:32 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+
+def readme():
+  with open('README.md', 'r') as f:
+    return f.read()
+
+
+setup(
+  name='vimeworld',
+  version='0.0.1',
+  author='xtraswed',
+  author_email='xtraswed@gmail.com',
+  description='SDK для работы с API VimeWorld',
+  long_description=readme(),
+  long_description_content_type='text/markdown',
+  url='https://github.com/xtraswed/vimeworld',
+  packages=find_packages(),
+  install_requires=['requests>=2.25.1'],
+  classifiers=[
+    'Programming Language :: Python :: 3.11',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent'
+  ],
+  keywords='vimeworld VimeWorld Vimeworld vimeWorld xtraswed Xtraswed minecraft api sdk SDK',
+  project_urls={
+    'GitHub': 'https://github.com/xtraswed'
+  },
+  python_requires='>=3.8'
+)
